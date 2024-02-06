e67955a05bda2a7868bc2729f00c0584d77900bf
yamtimor <yamtimor@gmail.com>
2024-02-06 16:58:02 +0200

    Creating a simple scanPort function

--- PortScanner.java
+++ PortScanner.java
@@ -1,2 +1,22 @@
+import java.net.Socket;
+import java.net.InetSocketAddress;
+
 public class PortScanner {
+
+    public static void scanPorts (String target, int startPort, int endPort){
+
+        for (int port = startPort; port <= endPort; port++) {
+            try (Socket socket = new Socket()) {
+                socket.connect(new InetSocketAddress(target, port, 1000));
+                System.out.println("Port" + port + "is open.");
+            } catch (Exception e) {
+                System.out.println("Port " + port + "Is closed.");
+            }
+        }
+    }
+
+    public static void main (String[] args)
+    {
+
+    }
 }
